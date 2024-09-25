8c305a1db1a917520f65b0455bd385afb4b046f4
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-09-25 20:06:28 +0500

    Returning HTML pages and Basic Routing

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer(() => {
-  console.log("Request Made");
+const server = http.createServer((res, req) => {
+  let path = "./view/";
+  switch (path) {
+    case "./":
+      path += "index.html";
+      break;
+    case "./about":
+      path += "About.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  res.setHeader("Content-Type", "text/html");
+
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log("There is an error: ", err);
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {});
