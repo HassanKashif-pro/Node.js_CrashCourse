d89506a7cf99b54d1f8d9c096257d68d981f2861
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-09-27 06:50:31 +0500

    Routing with HTML set

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ const express = require("express");
 
 const app = express(() => {});
 
+app.set("view engine", "ejs");
+
 app.listen(3000);
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.send("./views/index.html");
+});
+app.get("/about", (req, res) => {
+  res.sendFile("./views/About.html");
+});
+
+app.use("/404", (req, res) => {
+  res.status().sendFile("./views/404.html");
+});
