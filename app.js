d3a0fccca75f099ad204e416db95a42720f92271
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-09-29 19:18:05 +0500

    <% %> EJS functionality not working-

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
 const express = require("express");
 
-const app = express(() => {});
+// express app
+const app = express();
 
+// register view engine
 app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.listen(3000);
+// Middleware for serving static files
+app.use(express.static("public"));
+
+// listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 app.get("/", (req, res) => {
-  res.render("Index");
-});
-app.get("/about", (req, res) => {
-  res.render("About");
-});
-app.get("/blog/create", (req, res) => {
-  res.render("create");
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat Bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
-app.use("/404", (req, res) => {
-  res.status().render("404");
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
 });
